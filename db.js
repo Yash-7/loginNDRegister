5baa8d282e3eeff584f3f839799cb2d21c8cc60c
Yash-7 <yashwanthnaik10@gmail.com>
2018-06-23 15:01:59 +0530

    CRUD done

--- db.js
+++ db.js
@@ -2,15 +2,74 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 const url = 'mongodb://localhost:27017';
 const dbName='loginRegister';
+var mongodb = require('mongodb');
 
-module.exports = {
+//   read: function() {
+//     return MongoClient.connect(url).then(function(client) {
+//         const mydb=client.db(dbName);
+//         var collection = mydb.collection('users');
+//         return collection.find().toArray();
+//     })
+//   }
+// read: new Promise((resolve,reject)=>{
+//   MongoClient.connect(url).then((client)=>{
+//     console.log('read called')
+//     const mydb=client.db(dbName);
+//     var collection = mydb.collection('users');
+//     resolve(collection.find().toArray());
+//   })
+// })
+module.exports={
   read: function() {
     return MongoClient.connect(url).then(function(client) {
-        const mydb=client.db(dbName);
-        var collection = mydb.collection('users');
-        return collection.find().toArray();
-    }).then(function(items) {
-      return items;
-    });
+      console.log('read called')
+      const mydb=client.db(dbName);
+      var collection = mydb.collection('users');
+      return collection.find().toArray();
+    })
+  },
+  findById: function(id) {
+    return MongoClient.connect(url).then(function(client) {
+      console.log('read by id called')
+      const mydb=client.db(dbName);
+      var collection = mydb.collection('users');
+      return collection.find({
+        _id : new mongodb.ObjectID(id)
+      }).toArray();
+    })
+  },
+  insert: function(username, pwd){
+    return MongoClient.connect(url).then(function(client){
+      console.log('insert called')
+      const mydb=client.db(dbName);
+      var collection=mydb.collection('users');
+      collection.insertOne({
+        'username':username,
+        'password':pwd
+      })
+    })
+  },
+  delete: function(id){
+    return MongoClient.connect(url).then(function(client){
+      console.log('delete called')
+      const mydb=client.db(dbName);
+      var collection=mydb.collection('users');
+      collection.deleteOne({
+        '_id':new mongodb.ObjectID(id)
+      })
+    })
+  },
+  update: function(id,username,pwd){
+    return MongoClient.connect(url).then(function(client){
+      console.log('delete called')
+      const mydb=client.db(dbName);
+      var collection=mydb.collection('users');
+      collection.update({
+        '_id':new mongodb.ObjectID(id)
+      },{
+        username:username,
+        password:pwd
+      })
+    })
   }
-};
+}
--- routes/home.js
+++ routes/home.js
@@ -1,27 +1,57 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+const request=require('request');
 const app= require('../index.js').users;
 
 const db=require('../db')
 
-db.read().then(function(items) {
-    console.log('The promise was fulfilled with items!');
-    return items;
-}, function(err) {
-    console.error('The promise was rejected', err, err.stack);
-}).then(function(items){
-    console.log('why' , items)
-    router.get('/signup',(req,res)=>{
-        res.render('signup');
+router.get('/signup',(req,res)=>{
+    res.render('signup')
+})
+router.get('/users',(req,res)=>{
+    db.read().then(function(items){
+        res.render('users',{
+            data:items
+        });
     })
-    router.get('/login',(req,res)=>{
-        res.send(items)
-    })
-});
-
+})
 router.post('/signup',(req,res)=>{
-    console.log(req.body.username)
-    res.redirect('/login')
+    db.insert(req.body.username,req.body.password)
+    res.redirect('/users')
+})
+router.get('/login',(req,res)=>{
+    res.send('login page')
+})
+router.get('/delete/:id',(req,res)=>{
+    db.delete(req.params.id);
+    res.redirect('/users');
+})
+router.get('/users/:id',(req,res)=>{
+    db.findById(req.params.id).then(user=>{
+        res.render('eachUser',{
+            data:user
+        });
+    }).catch(err=>{
+        console.log(err);
+    })   
+})
+router.post('/edit',(req,res)=>{
+    db.update(req.body.id,req.body.username,req.body.password).then(()=>{
+        res.redirect('/users');
+    })
+})
+var apiPromise = new Promise((resolve, reject)=>{
+    request('http://open.spotify.com/track/6rqhFgbbKwnb9MLmUQDhG6', { json: true }, (err, response, body) => {
+        if (err) { return console.log(err); }
+        resolve(body);
+        // console.log(response)
+    })
+})
+apiPromise.then(function(value){
+    router.get('/api',(req,res)=>{
+        res.send(value);
+    })
 })
 
 router.get('',(req,res)=>{
